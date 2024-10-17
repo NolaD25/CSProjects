c59920649c6d1aa5b9da70fd57cf78c5b8da0346
NolaD25 <noladeroo25@marlborough.org>
2024-10-16 20:08:59 -0700

    Update electionMap24.js

--- electionMap24.js
+++ electionMap24.js
@@ -404,11 +404,11 @@ function mouse_box(state){
     textSize(14);
     fill(50);
     if(state == "Nebraska" || state == "Maine"){
-        text(ecVotesDem+ecVotesRep + " electoral votes(EV)", mouseX - 90, mouseY + 45);
+        text(ecVotesDem+ecVotesRep + " electoral votes (EV)", mouseX - 90, mouseY + 45);
         textSize(10);
         text("EV:", mouseX + 50, mouseY + 45);
     }else{
-        text(ecVotes + " electoral votes(EV)", mouseX - 90, mouseY + 45);
+        text(ecVotes + " electoral votes (EV)", mouseX - 90, mouseY + 45);
         textSize(10);
         text("EV:", mouseX + 50, mouseY + 45);
     }
