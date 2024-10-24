c4ae38fb0b3829bf06d24a73e7a9e871958c0905
NolaD25 <noladeroo25@marlborough.org>
2024-10-24 14:05:55 -0700

    Update electionMap24.js

--- electionMap24.js
+++ electionMap24.js
@@ -562,6 +562,12 @@ function draw() {
         */
         
     }
+    
+    textAlign(CENTER);
+    textSize(12);
+    fill(50);
+    textFont(fontRegular);
+    text("Results are as reported by The Associated Press at APNews.com.", width/2,height-20);
 
     fill(0, 0, 0);
     let x = 25;
@@ -577,10 +583,7 @@ function draw() {
         mouse_box(state);
     }
     
-    textAlign(CENTER);
-    textSize(12);
-    fill(50);
-    text("Results are as reported by The Associated Press at APNews.com.", width/2,height-20);
+    
 }
 
 
