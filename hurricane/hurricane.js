e16a7a6ccdb9378c76de871a81b2ec602f630e46
NolaD25 <noladeroo25@marlborough.org>
2025-03-18 11:39:47 -0700

    Update hurricane.js

--- hurricane/hurricane.js
+++ hurricane/hurricane.js
@@ -4,6 +4,25 @@ function setup(){
     colorMode(HSB);
     count = 0;
     ceiling = 1;
+    inter = 0;
+    i = 0;
+    t = 0;
+    storm_index = 0;
+
+}
+function draw_rect(j){
+
+    
+    x1 = map(storm_data[j].coords[i][0],10,30,0,1000);
+    y1 = map(storm_data[j].coords[i][1]*-1,90,200,0,1000);
+    x2 = map(storm_data[j].coords[i+1][0],10,30,0,1000);
+    y2 = map(storm_data[j].coords[i+1][1]*-1,90,200,0,1000);
+    
+    x = map(t,0,1,x1,x2);
+    y = map(t,0,1,y1,y2);
+        
+    
+    rect(x,y,10,10);
     
 }
 
@@ -12,8 +31,18 @@ function setup(){
 function draw(){
     background(0,100,0);
     noStroke();
-    if(count <= -1){
-        count = 0;
+    
+    
+    
+    draw_rect(storm_index);
+   
+    t += 0.05;
+    if(t > 1){
+        i++;
+        t = 0;
+    }
+    if(i > storm_data[storm_index].nums){
+        i = 0;
     }
     if(count >= storm_data.length){
         count = storm_data.length-1;
@@ -21,42 +50,39 @@ function draw(){
     //text(storm_data[0].coords[0],200,200);
     
     for(let i = count; i < count + ceiling; i++){
-        last_x = map(storm_data[i].coords[0][0],10,30,0,1000);
-        last_y = map(storm_data[i].coords[0][1]*-1,90,200,0,1000);
         for(let j = 0; j < storm_data[i].nums-1; j++){
-            //console.log(storm_data[i].name);
-            text(storm_data[i].name,200,100);
-            text(storm_data[i].coords.length,200,200);
-            text(storm_data[i].nums,200,300);
             stroke(i*5,75,100,);
             x = map(storm_data[i].coords[j][0],10,30,0,1000);
             y = map(storm_data[i].coords[j][1]*-1,90,200,0,1000);
-            //rect(x, y,10,10);
+            rect(x, y,10,10);
             
-            line(last_x, last_y, x, y);
-            last_x = x;
-            last_y = y;
         }
+
     }
-    //text(storm_data.length,200,200);
-    //text(count,200,400);
-   
+        
+    
+    
     
 }
 function keyPressed(){
     if(keyCode === RIGHT_ARROW){
-        count+=1;
-        ceiling = 1;
+        storm_index ++;
+        count ++;
+
     }
     if(keyCode === LEFT_ARROW){
-        count-=1;
-        ceiling = 1;
+        storm_index --;
+        count --;
         
     }
     if(keyCode === UP_ARROW){
-        count = 0;
-        ceiling = storm_data.length;
+        //count = 0;
+        //ceiling = storm_data.length;
+    
         
+    }
+    if(keyCode === DOWN_ARROW){
+
     }
     
 }
